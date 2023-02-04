bacad7f1219eb04b2cd8446c6aad02f634937e61
Logan Raffkind <logan.raffkind@gmail.com>
2023-02-04 14:22:50 -0800

    Screen file routing

---
+++ src/screens/index.tsx
@@ -0,0 +1,3 @@
+export { default as LoginScreen } from "./loginScreen/LoginScreen";
+export { default as HomeScreen } from "./homeScreen/HomeScreen";
+export { default as RegistrationScreen } from "./registrationScreen/RegistrationScreen";
